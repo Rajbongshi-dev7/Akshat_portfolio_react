5d23855883e312c5715caf13d40b2ad3e8db9d77
Doyal Rajbongshi <doyalrajbs7@gmail.com>
2026-04-21 23:19:41 +0600

    Ribbon status code completed

--- src/components/StatusRibbon/StatusRibbon.jsx
+++ src/components/StatusRibbon/StatusRibbon.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const StatusRibbon = () => {
   return (
     <div className="bg-[#1D2132] w-full px-16 md:px-24 lg:px-30">
-      <div className=" max-w-7xl mx-auto -mt-4 mb-20">
+      <div className="flex items-center max-w-7xl mx-auto gap-4 px-6 py-8">
+        <div className="h-px flex-1 bg-white/10"></div>
+        <span className="font-tech text-[10px] text-gray-400 uppercase tracking-[0.3em]">
+          System_Status
+        </span>
+        <div className="h-px flex-1 bg-white/10"></div>
+      </div>
+      {/* ribbon code */}
+      <div className=" max-w-7xl mx-auto mb-16">
         <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-4 gap-4">
           {/* 1. Hours Coded (The "Grind" Stat) */}
           <div className="bg-[#1c1c1e]/40 border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-blue-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all group">
@@ -16,7 +24,7 @@ const StatusRibbon = () => {
               </span>
               <span className="text-[10px] text-gray-500 font-tech">HRS</span>
             </div>
-            <p className="text-[9px] text-gray-500 uppercase mt-2 tracking-tighter">
+            <p className="text-[9px] text-gray-500 font-body uppercase mt-2 tracking-tighter">
               Hours spent in VS Code
             </p>
           </div>
@@ -34,7 +42,7 @@ const StatusRibbon = () => {
                 /PROD
               </span>
             </div>
-            <p className="text-[9px] text-gray-500 uppercase mt-2 tracking-tighter">
+            <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Production-ready builds
             </p>
           </div>
@@ -57,7 +65,7 @@ const StatusRibbon = () => {
                 </span>
               </div>
             </div>
-            <p className="text-[9px] text-gray-500 uppercase mt-2 tracking-tighter">
+            <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Year-round contribution
             </p>
           </div>
@@ -73,7 +81,7 @@ const StatusRibbon = () => {
               </span>
               <span className="text-[10px] text-gray-500 font-tech">GMT+6</span>
             </div>
-            <p className="text-[9px] text-gray-500 uppercase mt-2 tracking-tighter">
+            <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Available for remote sync
             </p>
           </div>
