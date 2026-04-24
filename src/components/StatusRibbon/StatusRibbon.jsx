e06a862e103c40d8a11fe59ae9eb61e044c92d9b
Doyal Rajbongshi <doyalrajbs7@gmail.com>
2026-04-25 00:53:43 +0600

    added framer motion animation on the ribbon cards

--- src/components/StatusRibbon/StatusRibbon.jsx
+++ src/components/StatusRibbon/StatusRibbon.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {motion} from "framer-motion"
 
 const StatusRibbon = () => {
   return (
@@ -11,10 +12,19 @@ const StatusRibbon = () => {
         <div className="h-px flex-1 bg-white/10"></div>
       </div>
       {/* ribbon code */}
-      <div className=" max-w-7xl mx-auto">
+      <div className=" max-w-7xl  mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-2 lg:grid-cols-4 gap-4">
           {/* 1. Hours Coded (The "Grind" Stat) */}
-          <div className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-blue-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all group">
+          <motion.div initial={{ opacity:0,scale:0.1 }}
+      
+    
+      whileInView={{ opacity: 1,scale:1 }}
+      
+      
+      transition={{ duration: .6,  ease:"easeIn"  }}
+      
+    
+      viewport={{ once:false, amount:.1 }} className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-blue-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all group">
             <span className="font-tech text-blue-500 text-[10px] uppercase tracking-widest mb-2">
               Total Focus
             </span>
@@ -27,10 +37,16 @@ const StatusRibbon = () => {
             <p className="text-[9px] text-gray-500 font-body uppercase mt-2 tracking-tighter">
               Hours spent in VS Code
             </p>
-          </div>
+          </motion.div>
 
           {/* 2. Projects Completed (The "Output" Stat) */}
-          <div className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-blue-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all">
+          <motion.div initial={{ opacity: 0,scale:0.1 }}
+      
+      whileInView={{ opacity: 1, scale:1 }}
+      
+      transition={{ duration: .6,  ease:"easeIn" }}
+      
+      viewport={{ once:false, amount:.1 }} className="bg-[#17171f] border  border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-blue-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all">
             <span className="font-tech text-blue-500 text-[10px] uppercase tracking-widest mb-2">
               Deployments
             </span>
@@ -45,10 +61,19 @@ const StatusRibbon = () => {
             <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Production-ready builds
             </p>
-          </div>
+          </motion.div>
 
           {/* 3. GitHub Contributions (The "Consistency" Stat) */}
-          <div className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-green-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all">
+          <motion.div initial={{ opacity: 0,scale:0.1 }}
+      
+   
+      whileInView={{ opacity: 1,scale:1 }}
+      
+     
+      transition={{ duration: .6, ease:"easeIn"  }}
+      
+      
+      viewport={{ once:false, amount:.1 }} className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-green-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all">
             <span className="font-tech text-green-500 text-[10px] uppercase tracking-widest mb-2">
               Git Activity
             </span>
@@ -68,10 +93,20 @@ const StatusRibbon = () => {
             <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Year-round contribution
             </p>
-          </div>
+          </motion.div>
 
           {/* 4. Active Node (The "Live" Stat) */}
-          <div className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-cyan-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all group">
+          <motion.div 
+      initial={{ opacity: 0,scale:0.1 }}
+      
+    
+      whileInView={{ opacity: 1, scale:1 }}
+      
+      
+      transition={{ duration: .6, ease:"easeIn" }}
+      
+    
+      viewport={{ once:false, amount:.1 }} className="bg-[#17171f] border border-white/5 rounded-3xl p-6 flex flex-col justify-center text-center hover:border-cyan-500/20 hover:shadow-blue-700 shadow-lg duration-400 hover:scale-y-110 transition-all group">
             <span className="font-tech text-cyan-400 text-[10px] uppercase tracking-widest mb-2">
               Active Node
             </span>
@@ -84,7 +119,7 @@ const StatusRibbon = () => {
             <p className="text-[9px] text-gray-500 font-body  uppercase mt-2 tracking-tighter">
               Available for remote sync
             </p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
